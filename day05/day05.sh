ddd0a50833c5386661d2f48a839a242a2554015e
Scott <se.brink15@gmail.com>
2018-12-09 03:57:34 -0500

    Done, mac needs bash4.0 :(

--- day05/day05.sh
+++ day05/day05.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 input=$(cat input.txt)
+size=${#input}
 
-for i in {1..1000}; do
-	for j in {a..z}; do
-		input=${input//${j^^}$j}
-		input=${input//$j${j^^}}
+# Just do 26 different rounds of part 1, ezpz
+for chr in {a..z}; do
+	tmpInput=${input//$chr}
+	tmpInput=${tmpInput//${chr^^}}
+	for i in {1..1000}; do
+		for j in {a..z}; do
+			tmpInput=${tmpInput//${j^^}$j}
+			tmpInput=${tmpInput//$j${j^^}}
+		done
 	done
+	if [ ${#tmpInput} -lt $size ]; then
+		$size=${#tmpInput}
+	fi
 done
 
-echo ${#input}
+echo $size
