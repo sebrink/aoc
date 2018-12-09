90a4f06a18adbb6a39665bd152790513ffe5cec6
Scott <se.brink15@gmail.com>
2018-12-09 03:33:10 -0500

    This breaks if run on bash3.X, only works on bash 4.0

--- day05/day05.sh
+++ day05/day05.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 input=$(cat input.txt)
-size=$(#input)
 
 for i in {1..1000}; do
 	for j in {a..z}; do
-		# Now I gotta do the actual stuff hmm react, time to learn formatting stuff
+		input=${input//${j^^}$j}
+		input=${input//$j${j^^}}
 	done
 done
 
-echo $(#input)
+echo ${#input}
