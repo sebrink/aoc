5b4af2a0b57f2ee4b12d71ddd7f945a570c827e5
Scott <se.brink15@gmail.com>
2019-12-03 09:56:07 -0500

    Finished part 2

--- 2019/day01/part2/day01.go
+++ 2019/day01/part2/day01.go
@@ -9,9 +9,14 @@ import (
 )
 
 // Does the fuel calculation
-func calcFuel(fuel int) int {
+func calcFuel(fuel, total int) int {
 	// Divide by 3, round down (go does this by default with a "/"), subtract 2
-	return ((fuel / 3) - 2)
+	// Implement recursive fuel checking until fuel amount is below 0
+	var newFuel = ((fuel / 3) - 2)
+	if newFuel <= 0 {
+		return total
+	}
+	return calcFuel(newFuel, total+newFuel)
 }
 
 // Processes input file
@@ -27,6 +32,7 @@ func fileProccessor(puzzle string) int {
 	// Take in input and process data
 	scanner := bufio.NewScanner(file)
 	var total = 0
+	var extraFuel = 0
 	for scanner.Scan() {
 		// Convert string to int
 		line, err := strconv.Atoi(scanner.Text())
@@ -34,7 +40,7 @@ func fileProccessor(puzzle string) int {
 			log.Fatal(err)
 		}
 		// Calculate the fuel and add it to the total
-		total += calcFuel(line)
+		total += calcFuel(line, extraFuel)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
