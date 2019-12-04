0475a7e07475916204e6ba3ef723c08e34fbf607
Scott <se.brink15@gmail.com>
2019-12-03 20:23:21 -0500

    Finished part2, I'm so glad I fixed my lack of efficiency

--- 2019/day02/part2/day02.go
+++ 2019/day02/part2/day02.go
@@ -10,16 +10,30 @@ import (
 // Incode Program
 // I literally made this code like 50 times faster, look at those old commits
 func intcodeProgram(array []int) int {
-	var i = 0
-	for array[i] != 99 {
-		// Cover opcode 1
-		if array[i] == 1 {
-			array[array[i+3]] = array[array[i+1]] + array[array[i+2]]
+
+	// Just brute force it :pog:
+	thisRun := make([]int, len(array))
+	for n := 0; n <= 99; n++ {
+		for v := 0; v <= 99; v++ {
+			copy(thisRun, array)
+			thisRun[1] = n
+			thisRun[2] = v
+			var i = 0
+			for thisRun[i] != 99 {
+				// Cover opcode 1
+				if thisRun[i] == 1 {
+					thisRun[thisRun[i+3]] = thisRun[thisRun[i+1]] + thisRun[thisRun[i+2]]
+				}
+				if thisRun[i] == 2 {
+					thisRun[thisRun[i+3]] = thisRun[thisRun[i+1]] * thisRun[thisRun[i+2]]
+				}
+				i += 4
+			}
+			if thisRun[0] == 19690720 {
+				fmt.Println(100*n + v)
+				break
+			}
 		}
-		if array[i] == 2 {
-			array[array[i+3]] = array[array[i+1]] * array[array[i+2]]
-		}
-		i += 4
 	}
 	return array[0]
 }
