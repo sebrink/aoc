05153a04a80694ea13408845945d2131c5388355
Scott <se.brink15@gmail.com>
2019-12-03 19:25:42 -0500

    Spaghetti code, long runtime, but it works lol, done part1

--- 2019/day02/part1/day02.go
+++ 2019/day02/part1/day02.go
@@ -5,30 +5,103 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 // Incode Program
-func intcodeProgram(list string) string {
-	//TODO
+// If you've ever thought you made spaghetti code, you'll realize nothing beats this function
+func intcodeProgram(array []string) string {
 
-	// Double check on the return and input type, not sure if those are correct
+	// Getting my actual values
+	var x, err1 = strconv.Atoi(array[1])
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	var y, err2 = strconv.Atoi(array[2])
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	var z, err3 = strconv.Atoi(array[3])
+	if err3 != nil {
+		log.Fatal(err3)
+	}
+	var addOne, errOne = strconv.Atoi(array[x])
+	if errOne != nil {
+		log.Fatal(errOne)
+	}
+	var addTwo, errTwo = strconv.Atoi(array[y])
+	if errTwo != nil {
+		log.Fatal(errTwo)
+	}
+	array[z] = strconv.Itoa(addOne + addTwo)
 
-	// Loop through the list in order to find each of the opcodes (they are 4 apart, positions 0,4,8,12,etc.)
+	for i := 0; i < len(array); i += 4 {
+		// Cover opcode 99
 
-	// Need a case for all of the following
+		if array[i] == "99" {
+			break
+		}
+		// Cover opcode 1
+		if array[i] == "1" {
 
-	// 1 is take next two positions add those and store at position of the 3rd
+			// Getting my actual values
+			var x, err1 = strconv.Atoi(array[i+1])
+			if err1 != nil {
+				log.Fatal(err1)
+			}
+			var y, err2 = strconv.Atoi(array[i+2])
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+			var z, err3 = strconv.Atoi(array[i+3])
+			if err3 != nil {
+				log.Fatal(err3)
+			}
+			var addOne, errOne = strconv.Atoi(array[x])
+			if errOne != nil {
+				log.Fatal(errOne)
+			}
+			var addTwo, errTwo = strconv.Atoi(array[y])
+			if errTwo != nil {
+				log.Fatal(errTwo)
+			}
+			array[z] = strconv.Itoa(addOne + addTwo)
+		}
 
-	// 2 is the same as 1 but multiply instead of add
+		// Cover opcode 2
+		if array[i] == "2" {
 
-	// 99 is stop (and return the total list)
+			// Getting my actual values
+			var x, err1 = strconv.Atoi(array[i+1])
+			if err1 != nil {
+				log.Fatal(err1)
+			}
+			var y, err2 = strconv.Atoi(array[i+2])
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+			var z, err3 = strconv.Atoi(array[i+3])
+			if err3 != nil {
+				log.Fatal(err3)
+			}
+			var mulOne, errOne = strconv.Atoi(array[x])
+			if errOne != nil {
+				log.Fatal(errOne)
+			}
+			var mulTwo, errTwo = strconv.Atoi(array[y])
+			if errTwo != nil {
+				log.Fatal(errTwo)
+			}
+			array[z] = strconv.Itoa(mulOne * mulTwo)
+		}
+	}
 
-	return "0"
+	return array[0]
 }
 
 // Processes input file
-func fileProccessor(puzzle string) int {
+func fileProccessor(puzzle string) string {
 
 	// Open file
 	file, err := os.Open(puzzle)
@@ -45,11 +118,8 @@ func fileProccessor(puzzle string) int {
 		input = (scanner.Text())
 	}
 	var list = strings.Split((input), ",")
-	fmt.Println(list)
-
-	// Pass it off to the function
-
-	return 0
+	fmt.Println(intcodeProgram(list[:]))
+	return ""
 }
 
 func main() {
